0f34ccd14d56f53e53e66262a8c58f309269280b
LHurle <ljhurle@ilstu.edu>
2021-09-02 11:21:52 -0500

    New Font Changes

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,8 +1,10 @@
-var f1;
+let f1, f2;
 
 function setup() {
   createCanvas(500, 500);
   f1 = loadFont("assets/BetterGrade.ttf");
+  f2 - loadFont("assets/Hakubo.ttf");
+  textAlign(CENTER);
 }
 
 function draw() {
@@ -10,5 +12,9 @@ function draw() {
   textFont(f1);
   textSize(100);
   text("hello world", 100, 100);
+
+  textFont(f2);
+  textSize(160);
+  text("Goodbye World", 100, 100);
 
 }
