d03fc31d4a8d5985dc22ebe2ec69128173e0778e
LHurle <ljhurle@ilstu.edu>
2021-09-28 11:43:18 -0500

    Touches

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,7 +1,14 @@
 let numberOfTouches ;
+let img1, img2, img3, img4;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 600);
+  img1 = loadImage("assets/desert.jpg");
+  img2 = loadImage("assets/threetree.jpg");
+  img3 = loadImage("assets/desertforest.jpg");
+  img4 = loadImage("assets/desertrocks.jpg");
+
+  imageMode(CENTER);
 }
 
 function draw() {
@@ -11,22 +18,23 @@ function draw() {
 
   switch(numberOfTouches) {
     case 0:
-      text("no one is touching the screen", 5, 22) ;
+      text("no one is touching the screen \nImage Credit: kjpargeter on freepik", 5, 22) ;
+      image(img1, 200, 200, 600, 300);
       break ;
 
       case 1:
-       text("it's kind of lonely here", 5, 22) ;
-      // put a picture here
+      text("it's kind of lonely here \nImage Credit: wirestock on freepik", 5, 22) ;
+      image(img2, 200, 200, 600, 300);
       break ;
 
       case 2:
-      text("two fingers are touching the screen", 5, 22) ;
-            // put a picture here
+      text("two fingers are touching the screen \nImage Credit: brgfx on freepik", 5, 22) ;
+      image(img3, 200, 200, 600, 300);
       break ;
 
       case 3:
-     text("three are touching the screen", 5, 22) ;
-            // put a picture here
+      text("three are touching the screen \nImage Credit: upklyak on freepik", 5, 22) ;
+      image(img4, 200, 200, 600, 300);
       break ;
 
 
