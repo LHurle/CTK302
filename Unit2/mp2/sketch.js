24d000f135ebf44a6306f31f2769990084c92da8
LHurle <ljhurle@ilstu.edu>
2021-09-30 11:44:04 -0500

    MP2 Progress

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,32 +1,33 @@
-let state = 0;
+let numberOfTouches;
+let img1, img2, img3;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1250);
+
+  img1 = loadImage("assets/MelanieAdlichRecreation.jpg");
+  img2 = loadImage("assets/Lukas.png");
+  img3 = loadImage("assets/Serpentine.png");
 }
 
 function draw() {
+  clear();
+  numberOfTouches = touches.length;
+  text(numberOfTouches + ' touches', 5, 10);
 
-  background(100);
-
-  switch (state) {
-
+  switch(numberOfTouches) {
     case 0:
-      text("0", 100, 100);
+      text("No One is Here \nArt Credit: Laura Hurley", 50, 50);
+      image(img1, 0, 100, 850, 1000);
       break;
 
     case 1:
-      text("1", 100, 100);
+      text("One Person is Here! \nArt Credit: Laura Hurley", 50, 50);
+      image(img1, 0, 100, 850, 1100);
       break;
 
     case 2:
-      text("2", 100, 100);
+      text("Two People are Here! \nArt Credit: Laura Hurley", 50, 50);
+      image(img1, 200, 200, 850, 1100);
       break;
-
   }
-}
-
-function mouseReleased() {
-  state++;
-  if (state > 2) state = 0;
-
 }
