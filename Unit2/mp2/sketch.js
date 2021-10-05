fd37552b5b65fbae5b4c2e578f413f83dff55e5a
LHurle <ljhurle@ilstu.edu>
2021-10-05 10:49:00 -0500

    MP2 Buttons Added

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -12,22 +12,47 @@ function setup() {
 function draw() {
   clear();
   numberOfTouches = touches.length;
+  fill("black");
   text(numberOfTouches + ' touches', 5, 10);
 
   switch(numberOfTouches) {
     case 0:
       text("No One is Here \nArt Credit: Laura Hurley", 50, 50);
       image(img1, 0, 100, 850, 1000);
+      fill("red");
+      rect(250, 10, 80, 80);
+      fill("white");
+      text("Touch Me!", 263, 50);
       break;
 
     case 1:
       text("One Person is Here! \nArt Credit: Laura Hurley", 50, 50);
       image(img1, 0, 100, 850, 1100);
+
+      fill("red");
+      rect(250, 10, 80, 80);
+      fill("white");
+      text("Touch Me!", 263, 50);
+
+      fill("red");
+      rect(350, 10, 80, 80);
+      fill("white");
+      text("Touch Me!", 363, 50);
       break;
 
     case 2:
       text("Two People are Here! \nArt Credit: Laura Hurley", 50, 50);
       image(img1, 200, 200, 850, 1100);
+
+      fill("red");
+      rect(250, 10, 80, 80);
+      fill("white");
+      text("Touch Me!", 263, 50);
+
+      fill("red");
+      rect(350, 10, 80, 80);
+      fill("white");
+      text("Touch Me!", 363, 50);
       break;
+    }
   }
-}
